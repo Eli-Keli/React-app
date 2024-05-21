04f7a249b9fe558391df9421aae247929a5f0b72
Eli Keli <muthokaelikeli@gmail.com>
2024-05-21 17:16:55 +0300

    Added list component

--- my-new-react-app/src/App.js
+++ my-new-react-app/src/App.js
@@ -3,14 +3,18 @@ import HelloWorld from './Hello';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
 import Login from './ConditionalComponent';
+import ListComponent from './ListComponent';
 
 function App() {
+  const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
   return (
     <div className='App'>
       <HelloWorld />
       <MyComponent />
       <Counter message="This is is a counter component with increment and decrement functionality!"/>
       <Login />
+      <ListComponent items={items}/>
     </div>
   )
 }
